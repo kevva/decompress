1231527ff32f867d73943a48daa0c462dabef844
Andrew Carpenter <andrew@audioinhd.com>
2016-11-16 17:21:07 -0500

    Use graceful-fs to prevent EMFILE issues

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const fs = require('fs');
+const fs = require('graceful-fs');
 const path = require('path');
 const decompressTar = require('decompress-tar');
 const decompressTarbz2 = require('decompress-tarbz2');
